33201fa25b4662c6e1579702edd707c6d9e0882f
ARPIT KUMAR TAPASWI <arpit.tapaswi@live.com>
2022-03-09 13:38:28 +0530

    Project Setup

---
+++ app/src/main/java/edu/califer/recuit_crmassignment/MainActivity.kt
@@ -0,0 +1,11 @@
+package edu.califer.recuit_crmassignment
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
